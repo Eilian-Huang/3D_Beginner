7485e890e702cea76009737fbd2db272a89bc1d1
Siyi-Huang <2829955641@qq.com>
2021-03-15 15:53:03 +0800

    Advance 4 finished

--- 3D Beginner/Assets/Scripts/Light/LightControl.cs
+++ 3D Beginner/Assets/Scripts/Light/LightControl.cs
@@ -1,33 +1,63 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class LightControl : MonoBehaviour
 {
-    public GameObject mlight;
+    public GameObject light;
     public float lightOnTimer;
     public float lightOffTimer;
 
     private float m_Timer = 0.0f;
     private bool isLightOn = true;
+    private GameObject Background;
+    private bool m_IsPlayerInRange;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "JohnLemon")
+        {
+            m_IsPlayerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "JohnLemon")
+        {
+            m_IsPlayerInRange = false;
+        }
+    }
 
     // Update is called once per frame
     void Update()
     {
-        m_Timer += Time.deltaTime;
+        if (m_IsPlayerInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            light.SetActive(!isLightOn);
+            isLightOn = !isLightOn;
+            m_Timer = 0;
+        }
+            m_Timer += Time.deltaTime;
         // Turn lights off when it's on for "LightOnTimer" seconds
         if (m_Timer >= lightOnTimer && isLightOn)
         {
-            mlight.SetActive(false);
+            light.SetActive(false);
             isLightOn = false;
             m_Timer = 0;
         }
         // Turn lights on when it's off for "LightOffTimer" seconds
         if (m_Timer >= lightOffTimer && !isLightOn)
         {
-            mlight.SetActive(true);
+            light.SetActive(true);
             isLightOn = true;
             m_Timer = 0;
+        }
+    }
+
+    void OnGUI()
+    {
+        if (m_IsPlayerInRange)
+        {
+            GUI.Box(new Rect(440, 120, 100, 24), "按E开关灯");
         }
     }
 }
